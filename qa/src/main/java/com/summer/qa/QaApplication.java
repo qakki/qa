139d3156434f8a098318de54ed304f7d347cef2b
lightingSummer <1356221057@qq.com>
2019-06-21 21:25:36 +0800

    add package

--- qa/src/main/java/com/summer/qa/QaApplication.java
+++ qa/src/main/java/com/summer/qa/QaApplication.java
@@ -3,11 +3,17 @@ package com.summer.qa;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.solr.SolrAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 @SpringBootApplication(exclude = SolrAutoConfiguration.class)
-public class QaApplication {
-
+public class QaApplication extends SpringBootServletInitializer {
   public static void main(String[] args) {
     SpringApplication.run(QaApplication.class, args);
+  }
+
+  @Override
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+    return builder.sources(QaApplication.class);
   }
 }
